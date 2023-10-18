7b3f525670de0a56bcc27852221f3fb3b7314d82
Nama Anda <email@example.com>
2023-10-18 14:50:35 +0700

    Kedua

--- script.js
+++ script.js
@@ -9,20 +9,17 @@ http.onload = function(){
         let data = JSON.parse(this.responseText);
         let output = "";
         for(let item of data){
-            output+= `
+            output += `
             <div class="data">
-            <p class="author"></p>
-            <p class="country"></p>
-            <img src= "${item.image}" alt ="${item.image}">
-            <p class="language"></p>
-            <link url= "${item.url}" alt ="${item.url}">
-            <p class="pages"></p>
-            <p class="title"></p>
-            <p class="year"></p>
-
-            </div>
-
-            ;`
+              <p class="author">${item.author}</p>
+              <p class="country">${item.country}</p>
+              <img src="${item.image}" alt="${item.title}">
+              <p class="language">${item.language}</p>
+              <a href="${item.url}" target="_blank" class="link">Link</a>
+              <p class="pages">${item.pages} pages</p>
+              <p class="title">${item.title}</p>
+              <p class="year">Published in ${item.year}</p>
+            </div>`;
         }
         document.querySelector(".data").innerHTML = output;
     }
